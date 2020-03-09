f4c15f9d71473badcb3c259ded70525c8462e669
mogmet <mogmet@gmail.com>
2020-03-09 23:38:23 +0900

    Revert "[update] ボタンを押したらボタンタイトルが変わるようになった"
    This reverts commit d880a6925c9e83a36b22bb6c73de6b85af6883d3.

--- app/src/main/java/com/mogmet/traveltest/MainActivity.kt
+++ app/src/main/java/com/mogmet/traveltest/MainActivity.kt
@@ -1,18 +1,12 @@
-package com.mogmet.traveltest.activity
+package com.mogmet.traveltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mogmet.traveltest.R
-import com.mogmet.traveltest.fragment.TopFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.frameLayout, TopFragment.newInstance())
-            .commit()
     }
 }
